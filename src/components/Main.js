94ff9e282d78e05baac2f8054f6ec54edb34cdff
Roman Trnka <trnka.roman13@gmail.com>
2022-02-10 16:09:20 +0100

    added styling - default

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,20 +2,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
 const Main = (props) => {
-  console.log(props.item)
-
   return (
     <main>
-      <div className="card">
-              <img src={`../images/${props.item.img}`} alt={props.item.place} />
-              <div className='card--stats'>
-                  <h6>{props.item.state}</h6>
-                  <a href={props.item.googleLink}>View on Google Maps</a>
-                  <h2>{props.item.place}</h2>
-                  <span>{props.item.date}</span>
-                  <p>{ props.item.description}</p>
-              </div>
+      <div className="main-card">
+        <img src={`../images/${props.item.img}`} alt={props.item.place} />
+        <div className="card--stats">
+          <div className="main-item1">
+            <h6>
+              <FontAwesomeIcon icon={faLocationDot} className="icon" />{' '}
+              {props.item.state}
+            </h6>
+            <a href={props.item.googleLink}>View on Google Maps</a>
+          </div>
+
+          <h2>{props.item.place}</h2>
+          <span>{props.item.date}</span>
+          <p>{props.item.description}</p>
+        </div>
       </div>
+      <hr />
     </main>
   )
 }
